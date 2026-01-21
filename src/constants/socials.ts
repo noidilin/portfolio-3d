35f9340569fcb09977489f56add0522834c42f97
noidilin <linganinja.0120@gmail.com>
2026-01-22 00:13:45 +0800

    chore: add constants data for social media url

---
+++ src/constants/socials.ts
@@ -0,0 +1,10 @@
+const SOCIALS = [
+  { name: 'Instagram', href: 'https://www.instagram.com/noidilin' },
+  {
+    name: 'Youtube',
+    href: 'https://www.youtube.com/@noidilin',
+  },
+  { name: 'GitHub', href: 'https://github.com/noidilin' },
+]
+
+export default SOCIALS
