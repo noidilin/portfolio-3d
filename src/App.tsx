3ee6cb282c0a35c23cbcd46f5e8fb4c0208cb2b0
noidilin <linganinja.0120@gmail.com>
2026-01-22 11:00:50 +0800

    feat(dep): add lenis for smoother scrolling in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import ReactLenis from 'lenis/react'
 import Hero from './sections/hero'
 import Nav from './sections/nav'
 
 function App() {
   return (
-    <div className="relative min-h-screen w-screen overflow-x-auto">
+    <ReactLenis root className="relative min-h-screen w-screen overflow-x-auto">
       <Nav />
       <Hero />
-    </div>
+    </ReactLenis>
   )
 }
 
